f7bde84b4a8e52cdb05833c613c69736da18a5e0
tinyAdapter <tinyadapter@outlook.com>
2024-09-20 21:33:56 +0800

    fix: arg type conflict with IOptionItem

--- src/configParser/configParser.ts
+++ src/configParser/configParser.ts
@@ -1,29 +1,26 @@
-import {argsParser} from "../scriptParser/argsParser";
+import { arg } from "../interface/sceneInterface";
+import { argsParser } from "../scriptParser/argsParser";
 
-interface IOptionItem{
-  key:string;
-  value:string | number | boolean;
-}
-interface IConfigItem{
-  command:string;
-  args:string[]
-  options:IOptionItem[];
+interface IConfigItem {
+  command: string;
+  args: string[]
+  options: arg[];
 }
 
 export type WebgalConfig = IConfigItem[]
 
 
-function configLineParser(inputLine:string):IConfigItem{
-  const options:Array<IOptionItem> = [];
+function configLineParser(inputLine: string): IConfigItem {
+  const options: Array<arg> = [];
   let command: string;
 
   let newSentenceRaw = inputLine.split(";")[0];
   if (newSentenceRaw === "") {
     // 注释提前返回
     return {
-      command:'',
-      args:[],
-      options:[]
+      command: '',
+      args: [],
+      options: []
     };
   }
   // 截取命令
@@ -43,18 +40,18 @@ function configLineParser(inputLine:string):IConfigItem{
   if (getOptionsResult) {
     const optionsRaw = newSentenceRaw.substring(getOptionsResult.index, newSentenceRaw.length);
     newSentenceRaw = newSentenceRaw.substring(0, getOptionsResult.index);
-    for (const e of argsParser(optionsRaw, (name,_)=>{return name;})) {
+    for (const e of argsParser(optionsRaw, (name, _) => { return name; })) {
       options.push(e);
     }
   }
   return {
     command,
-    args:newSentenceRaw.split('|').map(e=>e.trim()).filter(e=>e!==''),
+    args: newSentenceRaw.split('|').map(e => e.trim()).filter(e => e !== ''),
     options
   };
 }
 
-export function configParser(configText:string):WebgalConfig{
-  const configLines = configText.replaceAll(`\r`,'').split('\n');
-  return configLines.map(e=>configLineParser(e)).filter(e=>e.command!=='');
+export function configParser(configText: string): WebgalConfig {
+  const configLines = configText.replaceAll(`\r`, '').split('\n');
+  return configLines.map(e => configLineParser(e)).filter(e => e.command !== '');
 }
